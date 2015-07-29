3eed865b264783cee17a4c6ee9eb65a08cdb3136
jn2345 <jn2345@hawaii.edu>
2015-07-29 10:05:34 -1000

    Added Gitignore
    Ignores the data folder

---
+++ main.py
@@ -0,0 +1,5 @@
+from src.perdb import getDataObjects,loadPERdict
+
+perdict = loadPERdict()
+mtdict,keys,hoborhts = getDataObjects('CS',perdict)
+
--- perdb_analysis_0full.py
+++
@@ -1,5 +0,0 @@
-from bin.perdb import getDataObjects,loadPERdict
-
-perdict = loadPERdict()
-mtdict,keys,hoborhts = getDataObjects('CS',perdict)
-
